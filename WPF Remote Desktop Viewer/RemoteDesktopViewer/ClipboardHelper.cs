2582f9fb9260dedc70c481e2b5b722ecb2a50117
skyneton <67633420+skyneton@users.noreply.github.com>
2022-04-14 19:59:01 +0900

    STA

--- WPF Remote Desktop Viewer/RemoteDesktopViewer/ClipboardHelper.cs
+++ WPF Remote Desktop Viewer/RemoteDesktopViewer/ClipboardHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media;
 using RemoteDesktopViewer.Networks.Threading;
@@ -12,9 +13,11 @@ namespace RemoteDesktopViewer
         private object _beforeClipboardData;
 
         private bool _updateClipboard;
+        private Visual _visual;
 
         public void Create(Visual window)
         {
+            _visual = window;
             _clipboardManager?.Close();
             _clipboardManager = new ClipboardManager(window);
             _clipboardManager.AddCallback(ClipboardCallback);
@@ -29,27 +32,34 @@ namespace RemoteDesktopViewer
         private void ClipboardCallback()
         {
             if (!RemoteServer.Instance.ServerControl) return;
-            
-            var obj = Clipboard.GetDataObject();
-            var current = obj?.GetData(obj.GetFormats()[0]);
-            if (current == null || current.Equals(_beforeClipboardData)) return;
-            
-            _beforeClipboardData = current;
-            
-            if (_updateClipboard)
-            {
-                _updateClipboard = false;
-                return;
-            }
 
-            ClipboardThreadManager.Worker(RemoteServer.Instance.Broadcast, current);
+            try
+            {
+                var obj = Clipboard.GetDataObject();
+                var current = obj?.GetData(obj.GetFormats()[0]);
+                if (current == null || current.Equals(_beforeClipboardData)) return;
+
+                _beforeClipboardData = current;
+
+                if (_updateClipboard)
+                {
+                    _updateClipboard = false;
+                    return;
+                }
+
+                ClipboardThreadManager.Worker(RemoteServer.Instance.Broadcast, current);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
         }
 
         [STAThread]
         public void SetClipboard(object obj)
         {
             _updateClipboard = true;
-            Clipboard.SetDataObject(obj);
+            _visual.Dispatcher.Invoke(() => Clipboard.SetDataObject(obj));
         }
     }
 }
