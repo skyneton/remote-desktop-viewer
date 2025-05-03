6569b5c74ece666b9ae655192dfb682a01f0dea8
배동인 <67633420+skyneton@users.noreply.github.com>
2025-05-03 11:47:58 +0900

    update

--- Remote Deskop Control Pannel/ClientWindow.xaml.cs
+++ Remote Deskop Control Pannel/ClientWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace RemoteDeskopControlPannel
 {
@@ -8,9 +9,42 @@ namespace RemoteDeskopControlPannel
     public partial class ClientWindow : Window
     {
         public bool IsOpened;
+        private readonly int TopMenuOffsetX = 200;
+        private readonly int TopMenuOffsetY = 100;
         public ClientWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (WindowState != WindowState.Maximized)
+            {
+                TopMenu.Visibility = Visibility.Collapsed;
+                return;
+            }
+            var relPos = e.GetPosition(TopMenu);
+            if ((-TopMenuOffsetX < relPos.X && relPos.X - TopMenu.ActualWidth < TopMenuOffsetX)
+                && (-TopMenuOffsetY < relPos.Y && relPos.Y - TopMenu.ActualHeight < TopMenuOffsetY))
+                TopMenu.Visibility = Visibility.Visible;
+            else
+                TopMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized && WindowStyle != WindowStyle.None)
+            {
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Normal;
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            WindowStyle = WindowStyle.SingleBorderWindow;
         }
     }
 }
